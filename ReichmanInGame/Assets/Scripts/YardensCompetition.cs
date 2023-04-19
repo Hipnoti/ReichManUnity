d272543676497ca1066304079ccc60eb78075443
Lior Hadashian <lior@gavra-games.com>
2023-04-19 17:56:58 +0300

    Added YardensCompetition.cs

---
+++ ReichmanInGame/Assets/Scripts/YardensCompetition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YardensCompetition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Taunt();
+    }
+
+    void Taunt()
+    {
+        Debug.Log("You have a cannon? I got artillery");
+    }
+}

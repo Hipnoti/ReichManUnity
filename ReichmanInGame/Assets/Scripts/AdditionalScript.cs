844bf453f21a934b413c1681b1d60c5ffe71802e
Lior Hadashian <lior@gavra-games.com>
2023-04-19 18:12:19 +0300

    Revert "Merge branch 'Yarden.Cannon' of https://github.com/Hipnoti/ReichManUnity"
    This reverts commit 6196df3cb244b8a594b469c15e785a5e679ce33a, reversing
    changes made to 502d874efa47b17bb118fefd70bc03894176fbdc.

---
+++ ReichmanInGame/Assets/Scripts/AdditionalScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdditionalScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is Yarden's great script only in the his branch!");
+    }
+    
+}
